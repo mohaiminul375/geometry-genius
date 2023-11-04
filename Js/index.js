5cd8e094c61033a72e58b4e1534a6f036db0f43f
Mohaiminul375 <mohaiminul375@gmail.com>
2023-11-04 17:06:36 +0600

    calculate triangle

--- Js/index.js
+++ Js/index.js
@@ -1,4 +1,30 @@
-document.getElementById('blog').addEventListener('click',function(){
-    console.log('blog-pg')
-    window.location.href= 'blog.html'
+console.log('indexjs')
+
+//function for get input field value
+function getInputFieldValue(id){
+    const inputFieldString = document.getElementById(id).value;
+    const inputField = parseFloat(inputFieldString);
+    return inputField
+};
+
+//function for get text element value
+// function getTextElementValue(id){
+// const textElementString = document.getElementById(id).innerText;
+// const textElement = parseFloat(textElementString);
+// return textElement;
+// }
+
+
+//calculate triangle area volume
+document.getElementById('btn-triangle').addEventListener('click', function(){
+
+    //get input field value by function
+    const base = getInputFieldValue('triangle-b')
+    const height = getInputFieldValue('triangle-h')
+    
+    //get text element value by function
+    
+
+    const triangleCalculation = 0.5 * base * height;
+    console.log(triangleCalculation);
 })
