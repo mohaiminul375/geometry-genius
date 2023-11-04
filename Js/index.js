f5c99816605e00e2e20e7e166f4e4846b26e9e04
Mohaiminul375 <mohaiminul375@gmail.com>
2023-11-04 17:48:27 +0600

    calculate all formula

--- Js/index.js
+++ Js/index.js
@@ -14,23 +14,23 @@ const textElementString = document.getElementById(id).innerText;
 return textElementString;
 }
 
-
+let serial = 0;
 //set data table by function
-function setDataTable(gName, areaCalculation){
+function setDataTable(serial,gName, areaCalculation){
     
     const container = document.getElementById('table-container')
     const tr = document.createElement('tr');
     tr.innerHTML = `
-      <td>${1}</td>
+      <td>${serial}</td>
       <td>${gName}</td>
-      <td>${areaCalculation}cm<sup>2</sup></td>
+      <td>${(areaCalculation).toFixed(2)}cm<sup>2</sup></td>
       <td><button>Convert</button></td>
     `
     container.appendChild(tr);
 }
 //calculate triangle area volume
 document.getElementById('btn-triangle').addEventListener('click', function(){
-
+      serial += 1;
     //get input field value by function
     const base = getInputFieldValue('triangle-b')
     const height = getInputFieldValue('triangle-h')
@@ -42,12 +42,12 @@ document.getElementById('btn-triangle').addEventListener('click', function(){
     console.log(areaCalculation);
 
     //send data to table by function
-    setDataTable(gName, areaCalculation);
+    setDataTable(serial,gName, areaCalculation);
 });
 
 //calculate Rectangle area volume
 document.getElementById('btn-rectangle').addEventListener('click', function(){
-
+    serial += 1;
     //get input field value by function
     const length = getInputFieldValue('rectangle-l')
     const width = getInputFieldValue('rectangle-w')
@@ -59,5 +59,77 @@ document.getElementById('btn-rectangle').addEventListener('click', function(){
     console.log(areaCalculation);
 
     //send data to table by function
-    setDataTable(gName, areaCalculation);
+    setDataTable(serial,gName, areaCalculation);
+});
+
+
+//calculate Parallelogram area volume
+document.getElementById('btn-parallelogram').addEventListener('click', function(){
+    serial += 1;
+    //get input field value by function
+    const base = getInputFieldValue('parallelogram-b')
+    const height = getInputFieldValue('parallelogram-h')
+    
+    //get text element value by function
+    const gName = getTextElementValue('parallelogram-name');
+
+    const areaCalculation = base * height;
+    console.log(areaCalculation);
+
+    //send data to table by function
+    setDataTable(serial,gName, areaCalculation);
+});
+
+
+//calculate Rhombus area volume
+document.getElementById('btn-rhombus').addEventListener('click', function(){
+    serial += 1;
+    //get input field value by function
+    const d1 = getInputFieldValue('rhombus-d1')
+    const d2 = getInputFieldValue('rhombus-d2')
+    
+    //get text element value by function
+    const gName = getTextElementValue('rhombus-name');
+
+    const areaCalculation = 0.5 * d1 * d2;
+    console.log(areaCalculation);
+
+    //send data to table by function
+    setDataTable(serial,gName, areaCalculation);
+});
+
+//calculate pentagon area volume
+
+document.getElementById('btn-pentagon').addEventListener('click', function(){
+    serial += 1;
+    //get input field value by function
+    const p = getInputFieldValue('pentagon-p')
+    const b = getInputFieldValue('pentagon-b')
+    
+    //get text element value by function
+    const gName = getTextElementValue('pentagon-name');
+
+    const areaCalculation = 0.5 * p * b;
+    console.log(areaCalculation);
+
+    //send data to table by function
+    setDataTable(serial,gName, areaCalculation);
+});
+
+//calculate pentagon area volume
+
+document.getElementById('btn-ellipse').addEventListener('click', function(){
+    serial += 1;
+    //get input field value by function
+    const a = getInputFieldValue('ellipse-a')
+    const b = getInputFieldValue('ellipse-b')
+    
+    //get text element value by function
+    const gName = getTextElementValue('ellipse-name');
+
+    const areaCalculation = 3.14 * a * b;
+    console.log(areaCalculation);
+
+    //send data to table by function
+    setDataTable(serial,gName, areaCalculation);
 });
